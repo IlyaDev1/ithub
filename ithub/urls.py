f776d540b6a1c1ebb3328bf03502f93de794c304
guest <guest@robbit.local>
2024-06-16 17:48:27 +0300

    CreatedOgeApp

--- ithub/urls.py
+++ ithub/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+import oge.urls as oge_urls
 import mainapp.urls as main_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(main_app_urls))
+    path('', include(main_app_urls)),
+    path('oge/', include(oge_urls))
 ]
